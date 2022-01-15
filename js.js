94b96d355c5b6fec477fb9b79fb0a48a811cc776
hakue <adam.haldas@gmail.com>
2022-01-15 17:21:45 +0100

    add addItem function

--- js.js
+++ js.js
@@ -1,18 +1,33 @@
 // Selectors
-const addButton = document.querySelector('.form--button');
-const list = document.querySelector('.form--list');
-let input = document.querySelector('.form--input');
+const addButton = document.querySelector(".form--button");
+const list = document.querySelector(".form--list");
+let input = document.querySelector(".form--input");
 let itemId;
 //Functions
 const adddItem = (e) => {
   e.preventDefault();
-
+  //create div
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("todo");
   //create item
-  let newItem = document.createElement('li');
+  let newItem = document.createElement("li");
   newItem.textContent = input.value;
-  //add item to the todo list
-  input.value != 0 ? list.appendChild(newItem) : null;
-  input.value = '';
+  input.value = "";
+  //create confirm button
+  let confirmButton = document.createElement("button");
+  confirmButton.classList.add("todo--button__complete");
+  confirmButton.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+  //create delete button
+  let deleteButton = document.createElement("button");
+  deleteButton.classList.add("todo--button__delete");
+  deleteButton.innerHTML = ' <i class="fa-solid fa-trash"></i>';
+
+  //append Child
+  newDiv.appendChild(newItem);
+  newDiv.appendChild(confirmButton);
+  newDiv.appendChild(deleteButton);
+  list.appendChild(newDiv);
 };
+
 // Event Listeners
-addButton.addEventListener('click', adddItem);
+addButton.addEventListener("click", adddItem);
