735f7f318c50a37c63fc7164aa00eba413e933b7
Adam <adam.haldas@gmail.com>
2022-01-24 22:23:38 +0100

    add delete function

--- js.js
+++ js.js
@@ -1,25 +1,35 @@
+// 1. pobrac wszystkie elementy
+// 2. ustawi event lsitenery
+// 3. napisac dodaj item
+// 4. napisac usun item
+
+
+
 // Selectors
-const addButton = document.querySelector(".form--button");
-const list = document.querySelector(".form--list");
-let input = document.querySelector(".form--input");
-let itemId;
+const addButton = document.querySelector('.form--button');
+const list = document.querySelector('.form--list');
+let input = document.querySelector('.form--input');
+
+
+
 //Functions
 const adddItem = (e) => {
   e.preventDefault();
+  let itemId = new Date();
   //create div
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("todo");
+  let newDiv = document.createElement('div');
+  newDiv.classList.add('todo');
   //create item
-  let newItem = document.createElement("li");
+  let newItem = document.createElement('li');
   newItem.textContent = input.value;
-  input.value = "";
+  input.value = '';
   //create confirm button
-  let confirmButton = document.createElement("button");
-  confirmButton.classList.add("todo--button__complete");
+  let confirmButton = document.createElement('button');
+  confirmButton.classList.add('todo--button__complete');
   confirmButton.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
   //create delete button
-  let deleteButton = document.createElement("button");
-  deleteButton.classList.add("todo--button__delete");
+  let deleteButton = document.createElement('button');
+  deleteButton.classList.add('todo--button__delete');
   deleteButton.innerHTML = ' <i class="fa-solid fa-trash"></i>';
 
   //append Child
@@ -29,5 +39,20 @@ const adddItem = (e) => {
   list.appendChild(newDiv);
 };
 
+const deleteCheck = (e) => {
+  e.preventDefault();
+  const item = e.target;
+  console.log(item)
+  if(item.classList.contains('todo--button__delete')){
+    const todo = item.parentElement;
+    todo.remove();
+  }
+
+
+};
+
+
+
 // Event Listeners
-addButton.addEventListener("click", adddItem);
+addButton.addEventListener('click', adddItem);
+list.addEventListener('click', deleteCheck);
